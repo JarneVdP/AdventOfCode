807bc912e5c639cba097ca592a82f37b08da4ec5
Jarne <jarne@van-der-plas.net>
2022-03-27 00:04:24 +0100

    bruh

---
+++ 2021/Day 2/2_21.jl
@@ -0,0 +1,11 @@
+#=
+function position(input)
+    return input[1] + input[2]
+end
+=#
+
+
+input = readlines("test.txt")
+print(input)
+#inputdirection, inputdistance = input.split(" ")
+#print(inputdirection)
