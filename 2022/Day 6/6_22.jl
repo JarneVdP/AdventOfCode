65f4cf578eb8223cf90e9b318d239d8f6955afcb
Jarne <jarne@van-der-plas.net>
2022-12-06 20:24:43 +0100

    Use allunique :)

---
+++ 2022/Day 6/6_22.jl
@@ -0,0 +1,22 @@
+function packet(file, second::Bool)
+    string = readlines(file)
+    #declare a vector of length 4
+    #read the first 4 chars and place in charSeen
+    charArray = []
+    #loop trough each char of line and append to char
+    [push!(charArray, char) for char in collect(string[1])]
+    if !second
+        for i in 1:length(charArray)
+            allunique(charArray[i:i+4]) ? [return i+3] : nothing
+        end
+    else
+        for i in 1:length(charArray)
+            println(charArray[i:i+13])
+            allunique(charArray[i:i+13]) ? [return i+13] : nothing
+        end
+    end
+end
+
+
+@show packet("2022/Day 6/6_22.txt", false)  #WSFTMRHPP
+@show packet("2022/Day 6/6_22.txt", true)  #WSFTMRHPP
