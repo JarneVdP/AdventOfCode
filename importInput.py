a89be939c784a871c08b78a8bd30606098ffa7d3
Jarne <jarne@van-der-plas.net>
2023-06-28 23:17:23 +0200

    automate getting data from the site

---
+++ importInput.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+# Get the URL for the input data from the user
+url = input("Enter the URL for the input data: ")
+
+# Extract the year and day from the URL eg https://adventofcode.com/2022/day/8 -> 2022, 8
+year = url.split("/")[3]
+day = url.split("/")[5]
+
+# Create the folder structure based on the year and day https://adventofcode.com/2022/day/8 -> main folder 2022 -> subfolder day8
+folder_name = os.path.join(year, f"Day {day}")
+os.makedirs(folder_name, exist_ok=True)
+
+# Puzzle inputs differ by user. So export your session ID as an environment variable and use it to authenticate your request
+with open("session.txt", "r") as f:
+    session_id = f.read().strip()
+
+# Make a GET request to the URL and get the input data
+inputurl = f"{url}/input"
+response = requests.get(inputurl, headers={"User-Agent": "https://github.com/JarneVdP/AdventOfCode by Jarne"}, cookies={"session": session_id})
+input_data = response.text
+
+# Write the input data to a file named data.txt in the created folder
+with open(os.path.join(folder_name, "data.txt"), "w") as f:
+    f.write(input_data)
